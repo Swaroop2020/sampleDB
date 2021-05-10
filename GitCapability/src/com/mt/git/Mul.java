7efd97c6780e7606b26d0921a8c8496b165fa233
SwaroopReddy <swaroop8787@gmail.com>
2021-05-10 22:39:15 +0530

    branch 1 modified

--- GitCapability/src/com/mt/git/Mul.java
+++ GitCapability/src/com/mt/git/Mul.java
@@ -2,7 +2,7 @@ package com.mt.git;
 
 public class Mul {
 	public static void main(String[] args) {
-		int a=10,b=20,c=30,d=40;
-		System.out.println("mul : "+a*b*c*d);
+		int a=10,b=20,c=30,d=40,e=50,f=60;
+		System.out.println("mul : "+a*b*c*d*e*f);
 	}
 }
