2c7068bb923092051a226ce74258cce10310275e
SwaroopReddy <swaroop8787@gmail.com>
2021-05-11 12:24:14 +0530

    up to date

--- GitCapability/src/com/mt/lit/Division.java
+++ GitCapability/src/com/mt/lit/Division.java
@@ -2,8 +2,8 @@ package com.mt.lit;
 
 public class Division {
 	public static void main(String[] args) {
-		int a=20,b=10;
-		int result = a/b;
+		int a=20,b=10,c=5;
+		int result = a/b/c;
 		System.out.println(result);
 	}
 }
